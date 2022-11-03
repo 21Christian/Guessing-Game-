c19674503d39b57e0998fb562792e7c795e6eef6
Kristian Perriu <92625983+21Christian@users.noreply.github.com>
2022-11-03 17:21:00 +0100

    Update guessing_game.cpp

--- guessing_game.cpp
+++ guessing_game.cpp
@@ -3,25 +3,38 @@ using namespace std;
 
 
 void guessing_game(){
+    // variables 
     int chances = 9;
     bool running = true;
     int random_num = rand() % 101;
     int guessed_number;
     cout<<"Enter your number: ";
     cin>>guessed_number; 
+    
+    // main loop
     while(running){
+        
+        // checking if the number is larger 
         if(guessed_number > random_num){
             cout<<"The number guessed is too big, you have "<<chances<<" chances left."<<endl;
             cout<<"Enter your number: ";
             cin>>guessed_number;
+           // reducing the chances since the guess is wrong 
             chances--;
+            
+        // checking if the number is smaller
         }else if(guessed_number < random_num){
             cout<<"The number guessed is too small, you have "<<chances<<" chances left."<<endl;
             cout<<"Enter your number: ";
             cin>>guessed_number;
+            // reducing the chances since the guess is wrong
             chances--;
+            
+        // checking if the numbers are equal
         } else if(guessed_number == random_num){
             cout<<"You found the number. You win!!!";
+        
+  
         }if (chances  <= 0){
             cout<<"You are out of chances.";
             running = false;
@@ -31,7 +44,7 @@ void guessing_game(){
 
 } 
 
-
+// main window 
 int main()
 {
     guessing_game();
